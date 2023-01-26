50b7c075c6ff844573974acb795cafbf95f59dd0
muaazmuhamed <118731594+muaazmuhamed@users.noreply.github.com>
2023-01-26 19:01:18 +0200

    add a teal color into panel

--- login.Designer.cs
+++ login.Designer.cs
@@ -57,7 +57,7 @@
             this.panel1.TabIndex = 0;
             // 
             // label1
-            //  
+            // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
