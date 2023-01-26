6efa836194556cbf75e3106af587f7c025c9648b
muaazmuhamed <118731594+muaazmuhamed@users.noreply.github.com>
2023-01-26 18:51:03 +0200

    Adjust font size

--- login.Designer.cs
+++ login.Designer.cs
@@ -25,7 +25,7 @@
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
-        /// </summary>
+        /// </summary> 
         private void InitializeComponent()
         { 
             this.panel1 = new System.Windows.Forms.Panel();
