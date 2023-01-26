718ae0f17e32e47eac58be5b3098d568f7ffa741
muaazmuhamed <118731594+muaazmuhamed@users.noreply.github.com>
2023-01-26 18:15:50 +0200

    add a picture box

--- login.Designer.cs
+++ login.Designer.cs
@@ -64,7 +64,7 @@
             this.label1.Location = new System.Drawing.Point(477, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(543, 36);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 0; 
             this.label1.Text = "Employee Management System Version 1.0"; 
             // 
             // label2
