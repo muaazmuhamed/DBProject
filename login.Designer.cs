b24656b89f78e999681ce3c69c79685a83a44a9d
muaazmuhamed <118731594+muaazmuhamed@users.noreply.github.com>
2023-01-26 18:51:46 +0200

    Adjust font color

--- login.Designer.cs
+++ login.Designer.cs
@@ -76,7 +76,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(239, 36);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Manage Employee";
+            this.label2.Text = "Manage Employee"; 
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
